53e6ea74dc0e673220ac11410229ec47cf9e3c8b
Jacobo de Haro <jacobodeharo@gmail.com>
2015-10-20 12:39:21 +0200

    License header added to all files.

--- ARehabSDK/GLTexture.h
+++ ARehabSDK/GLTexture.h
@@ -1,3 +1,24 @@
+/*
+This file is part of ARehab.
+
+ARehab is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+ARehab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2015 Jacobo de Haro
+jacobodeharo@gmail.com
+@jacobodeharo
+
+*/
+
 #ifndef GLTEXTURE_H_
 #define GLTEXTURE_H_
 
--- ARehabSDK/QARehabFileControlWidget.cpp
+++ ARehabSDK/QARehabFileControlWidget.cpp
@@ -1,4 +1,25 @@
-﻿#include "qarehabfilecontrolwidget.h"
+﻿/*
+This file is part of ARehab.
+
+ARehab is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+ARehab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2015 Jacobo de Haro
+jacobodeharo@gmail.com
+@jacobodeharo
+
+*/
+
+#include "qarehabfilecontrolwidget.h"
 
 namespace ARehabUI
 {
